a704d3f507a7de41f6bd0be46184072538dede54
joemanix65 <joe_manix@msn.com>
2021-09-01 22:05:05 +0200

    like dislike

--- app.js
+++ app.js
@@ -1,15 +1,19 @@
 const express = require('express'); 
 const mongoose = require('mongoose');
+const path = require('path');
 
 const userRoutes = require('./routes/user');
 const sauceRoutes = require('./routes/sauce');
 
+<<<<<<< Updated upstream
 mongoose.connect('mongodb+srv://@cluster0.mc40q.mongodb.net/test?retryWrites=true&w=majority',
+=======
+mongoose.connect('mongodb+srv://newUser26:42xgMdb65@cluster0.mc40q.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
+>>>>>>> Stashed changes
   { useNewUrlParser: true,
     useUnifiedTopology: true })
   .then(() => console.log('Connexion à MongoDB réussie !'))
   .catch(() => console.log('Connexion à MongoDB échouée !'));
-
 
 const app = express(); 
 
@@ -21,11 +25,7 @@ app.use((req, res, next) => {
   });
 
 app.use(express.json());
-
-app.use((req, res) => { 
-    res.json({ message: 'Votre requête a bien été reçue !' }); 
-    }); 
-
+app.use('/images', express.static(path.join(__dirname, 'images')));  
 app.use('/api/sauces', sauceRoutes);
 app.use('/api/auth', userRoutes);
      
